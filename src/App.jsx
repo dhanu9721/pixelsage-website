c5ee6b66df122a7ea5bfb301a0ea667353faa391
Dhananjay Maurya <91829244+dhanu9721@users.noreply.github.com>
2025-04-27 23:30:18 +0530

    change 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import AboutUs from './components/AboutUs';
@@ -10,34 +9,34 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 import Process from './components/Process';
-import Games from './components/Games';  // ✅ Import your Games page
+import Games from './components/Games';
 import './css/style.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/pixelsage-website">
       <Navbar />
       <Routes>
         <Route path="/" element={
           <>
             <Hero />
-            <Projects />
-            <Services />
-            <Portfolio />
-            <Process />
-            <AboutUs />
-            <Contact />
+            <div id="aboutus"><AboutUs /></div>
+            <div id="services"><Services /></div>
+            <div id="projects"><Projects /></div>
+            <div id="portfolio"><Portfolio /></div>
+            <div id="process"><Process /></div>
+            <div id="contact"><Contact /></div>
             <footer>
               <p>&copy; 2025 PixelSage. All rights reserved.</p>
             </footer>
           </>
         } />
+        
+        {/* Games Page */}
         <Route path="/games" element={<Games />} />
       </Routes>
     </Router>
   );
 }
-
-
 
 export default App;
